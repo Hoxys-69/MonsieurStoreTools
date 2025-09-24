da20baf92c572edf52993a0d14050c1d0e08ec87
Hoxys <30727923+Hoxys-69@users.noreply.github.com>
2025-09-24 09:01:34 +0200

    Create data.js

---
+++ data.js
@@ -0,0 +1,24 @@
+const data = [
+  {
+    marque: "Somfy",
+    modele: "LT50",
+    procedure: [
+      "Appuyer 5 secondes sur le bouton PROG jusqu’à ce que le moteur réagisse.",
+      "Monter le volet jusqu’à la position haute désirée.",
+      "Appuyer brièvement sur PROG pour enregistrer.",
+      "Descendre le volet jusqu’à la position basse désirée.",
+      "Appuyer de nouveau sur PROG pour enregistrer."
+    ]
+  },
+  {
+    marque: "Bubendorff",
+    modele: "ID2",
+    procedure: [
+      "Couper l’alimentation pendant 2 secondes.",
+      "Rétablir l’alimentation pendant 7 secondes.",
+      "Recouper l’alimentation 2 secondes.",
+      "Rétablir l’alimentation : le volet fait un va-et-vient.",
+      "Procéder à la programmation des fins de course."
+    ]
+  }
+];
