53ec9bd4fd363958060c360090c7a4d264186bcf
Hoxys <30727923+Hoxys-69@users.noreply.github.com>
2025-09-24 12:53:33 +0200

    Update manifest.js

--- manifest.js
+++ manifest.js
@@ -3,13 +3,9 @@
   "short_name": "Volets",
   "start_url": "index.html",
   "display": "standalone",
-  "background_color": "#ffffff",
-  "theme_color": "#007aff",
+  "background_color": "#f4f4f4",
+  "theme_color": "#ff6600",
   "icons": [
-    {
-      "src": "icon.png",
-      "sizes": "192x192",
-      "type": "image/png"
-    }
+    { "src": "icon.png", "sizes": "192x192", "type": "image/png" }
   ]
 }
