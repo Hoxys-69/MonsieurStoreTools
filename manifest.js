914bf3b046493e89ce9167a4e14e0d09e6daca71
Hoxys <30727923+Hoxys-69@users.noreply.github.com>
2025-09-24 09:01:57 +0200

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,15 @@
+{
+  "name": "Guide Volets Roulants",
+  "short_name": "Volets",
+  "start_url": "index.html",
+  "display": "standalone",
+  "background_color": "#ffffff",
+  "theme_color": "#007aff",
+  "icons": [
+    {
+      "src": "icon.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    }
+  ]
+}
